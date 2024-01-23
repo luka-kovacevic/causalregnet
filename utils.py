5be9e60ff9806a04b1ea5d106e6e196cfbb664c9
Luka Kovacevic <luka.1kovacevic@gmail.com>
2024-01-23 17:23:03 +0000

    Added basic functionality to utils script.

---
+++ utils.py
@@ -0,0 +1,114 @@
+"""
+Copyright (c) 2024 Luka Kovacevic
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+"""
+
+import numpy as np
+import pandas as pd
+import igraph as ig
+import random
+
+from scipy.special import expit as sigmoid
+
+def set_random_seed(seed):
+    random.seed(seed)
+    np.random.seed(seed)
+
+def is_dag(W):
+    G = ig.Graph.Weighted_Adjacency(W.tolist())
+    return G.is_dag()
+
+def generate_dag(d, m, graph_type='ER'):
+    """Simulate random DAG with d nodes and m edges.
+
+    Args:
+        d (int): num of nodes
+        m (int): expected num of edges
+        graph_type (str): ER, SF, BP
+
+    Returns:
+        B (np.ndarray): [d, d] binary adj matrix of DAG
+    """
+
+    def _random_permutation(M):
+        # np.random.permutation permutes first axis only
+        P = np.random.permutation(np.eye(M.shape[0]))
+        return P.T @ M @ P
+
+    def _random_acyclic_orientation(B_und):
+        return np.tril(_random_permutation(B_und), k=-1)
+
+    def _graph_to_adjmat(G):
+        return np.array(G.get_adjacency().data)
+    
+    if graph_type == 'ER':
+        # Erdos-Renyi
+        G_und = ig.Graph.Erdos_Renyi(n=d, m=m)
+        B_und = _graph_to_adjmat(G_und)
+        B = _random_acyclic_orientation(B_und)
+    elif graph_type == 'ER-p':
+        G_und = ig.Graph.Erdos_Renyi(n=d, p=m)
+        B_und = _graph_to_adjmat(G_und)
+        B = _random_acyclic_orientation(B_und)
+    elif graph_type == 'SF':
+        # Scale-free, Barabasi-Albert
+        G = ig.Graph.Barabasi(n=d, m=int(round(m / d)), directed=True)
+        B = _graph_to_adjmat(G)
+    elif graph_type == 'BP':
+        # Bipartite, Sec 4.1 of (Gu, Fu, Zhou, 2018)
+        top = int(0.2 * d)
+        G = ig.Graph.Random_Bipartite(top, d - top, m=m, directed=True, neimode=ig.OUT)
+        B = _graph_to_adjmat(G)
+    else:
+        raise ValueError('unknown graph type')
+    B_perm = _random_permutation(B)
+    assert ig.Graph.Adjacency(B_perm.tolist()).is_dag()
+    return B_perm
+
+def generate_W(B, w_ranges=((-2.0, -0.5), (0.5, 2.0)), balanced=False):
+    """Simulate weight edges given a binary DAG adjacency matrix.
+
+    Args:
+        B (np.ndarray): [d, d] binary adj matrix of DAG
+        w_ranges (tuple): disjoint weight ranges
+
+    Returns:
+        W (np.ndarray): [d, d] weighted adj matrix of DAG
+    """
+    assert(is_dag(B))
+
+    W = np.zeros(B.shape)
+
+    if balanced:
+        s = np.random.randint(len(w_ranges), size=np.sum(B > 0))  # which range
+        S = np.zeros(B.shape)
+
+        entry = 0
+        for i,j in zip(*B.nonzero()):
+            S[i,j] = s[entry]
+            entry += 1
+    else:
+        S = np.random.randint(len(w_ranges), size=B.shape)
+
+    for i, (low, high) in enumerate(w_ranges):
+        U = np.random.uniform(low=low, high=high, size=B.shape)
+        W += B * (S == i) * U
+    return W
